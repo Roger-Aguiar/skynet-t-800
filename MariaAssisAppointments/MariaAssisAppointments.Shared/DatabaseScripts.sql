10d109f9a3e2c20c473809ae66d1c405c377a394
rogervisualstudio <rogervisualstudio@yahoo.com.br>
2023-08-04 21:15:08 -0300

    It creates a method to get just the pendent customers

---
+++ MariaAssisAppointments/MariaAssisAppointments.Shared/DatabaseScripts.sql
@@ -0,0 +1,14 @@
+use bafigorerp1bxqafiyd7;
+select * from User;
+select * from Customer;
+select * from Customer where UserId = 2;
+
+update Customer set Code = 'd73dc98d-5f03-4c31-841c-c8e37d9d972c' where PeopleId = 30 and UserId = 1;
+select * from Customer where UserId = 1 order by Pac;
+
+delete from Customer where PeopleId = 3 and UserId = 2;
+
+update User set LinkPac = '9a5672b8-af38-4ae5-b99c-f886adb09e5f' where UserId = 2;
+
+-- Código de pac atualizado
+-- 9a5672b8-af38-4ae5-b99c-f886adb09e5f
